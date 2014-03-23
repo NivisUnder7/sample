0d7e2bdb7bc845e333c218756add8629a976ba23
NivisUnder7 <tatsushi58@gmail.com>
2014-03-23 22:46:57 +0900

    特定の相手に反応するコードを追加

--- skyping_taro.rb
+++ skyping_taro.rb
@@ -14,12 +14,15 @@ loop do
       last_time = m.time.to_i
 	  next unless Time.now.to_i - last_time < hearing_threshold
 			
-	    chat.post "pong"if m.body.include? "ping"
+	    #chat.post "pong"if m.body.include? "ping"
 	    chat.post "そろそろ混ぜろよ" if m.body.include? "池田"
 	    chat.post "カナちゃんのこと呼んだし？" if m.body.include? "天狗王"
 	    chat.post "http://wikiwiki.jp/imgtank/?Covenanter" if m.body.include? "かなちゃん"
 			
-	    #chat.post "popopopong"  if message.user ="magi_hubot" &&
+	    if m.user.include? "magi_hubot" then
+	    	  sleep 1
+	    	  chat.post "聞こえなかったか？　ロンだ。そのポン成立せず・・・・・・！"
+	    	end
 
 		last_id = m.id
     end
